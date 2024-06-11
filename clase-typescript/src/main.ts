44b974a258616c19f5dd8f0e8c7200717a340e5a
Alonso Chavarria <alons182@hotmail.com>
2024-06-11 15:00:49 -0600

    chore: Enable console logging for each post in the arrPosts array

--- clase-typescript/src/main.ts
+++ clase-typescript/src/main.ts
@@ -5,10 +5,10 @@ async function init(){
   getPokemon();
   const arrPosts = await getPosts();
   
-  // arrPosts.forEach(post => {
-  //   console.log(post);
+  arrPosts.forEach(post => {
+    console.log(post);
     
-  // });
+  });
 }
 
 init();
@@ -48,22 +48,3 @@ function hola(name: string) {
 console.log(hola('mundo'));
 
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
